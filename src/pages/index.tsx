98cf076094f6a3ac5d52796183779b9a5fe153a9
Jamie Murphy <hello@jamiethalacker.dev>
2022-05-13 23:39:40 -0700

    banner hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,10 +20,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={`Homepage`} description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
